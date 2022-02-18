80726723528dee7a00208065090a959d9e506eb1
SquishyWizard <cthoma25@terpmail.umd.edu>
2022-02-18 17:03:05 -0500

    updates

---
+++ tetris/script.js
@@ -0,0 +1,8 @@
+const lTetromino = 'firstShape';
+const zTetromino = 'secindShape';
+const oTetromino = 'thirdShape';
+const iTetromino = 'fourthShape';
+const tTetromino = 'fifthShape';
+
+const tetrominos = [lTetromino,zTetromino,oTetromino,iTetromino,tTetromino];
+console.log(tetrominos[5]);
