b21f440b8f9408141676208576384336db8b29a4
SquishyWizard <cthoma25@terpmail.umd.edu>
2022-02-18 19:43:23 -0500

    Update script.js

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -3,6 +3,12 @@ const zTetromino = 'secindShape';
 const oTetromino = 'thirdShape';
 const iTetromino = 'fourthShape';
 const tTetromino = 'fifthShape';
-
 const tetrominos = [lTetromino,zTetromino,oTetromino,iTetromino,tTetromino];
 console.log(tetrominos[5]);
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid')
+  const width = 10
+  let squares = Array.from(document.querySelectorAll('grid div'))
+  const ScoreDisplay = document.querySelector('#score')
+  const StartBtn = document.querySelector('#start-button')
+});
