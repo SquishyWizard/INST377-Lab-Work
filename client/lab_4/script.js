762c9cbc720da94b2455cc30059f124674947a1c
SquishyWizard <cthoma25@terpmail.umd.edu>
2022-02-22 17:11:31 -0500

    Update script.js

---
+++ client/lab_4/script.js
@@ -0,0 +1,15 @@
+let slidePosition = 0;
+const slides = document.getElementsByClassName('carousel_item');
+const totalSlides = slides.length;
+
+document.
+  getElementById('carousel_button--next')
+  .addEventListener("click", function() {
+     moveToNextSlide();
+  });
+
+document.
+  getElementById('carousel_button--next')
+  .addEventListener("click", function() {
+      updateSlidePosition(moveToNextSlide);
+  });
