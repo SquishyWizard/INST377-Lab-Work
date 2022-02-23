692ddc23c6d19d75a8f03264408b03204c691aec
SquishyWizard <cthoma25@terpmail.umd.edu>
2022-02-23 13:24:18 -0500

    Update script.js

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -16,8 +16,11 @@ document.
 
   function updateSlidePosition(){
       for (let slide of slides) {
-          console.log(slide)
+          slide.classList.remove('carousel_item--visible');
+          slide.classList.add('carousel_item--hidden');
       }
+
+      slides[slidePosition].classList.add('carousel_item--visible')
   }
   
   function moveToNextSlide() {
