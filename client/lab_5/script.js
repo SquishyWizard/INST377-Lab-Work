a1b8c82cc1e24d55437b9116b0e6a62080e92479
SquishyWizard <cthoma25@terpmail.umd.edu>
2022-03-24 11:24:13 -0400

    Update script.js

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -1,14 +1,9 @@
-const arr = [];
-for (let i = 0; i<1000; i++){
-  arr.push(element[i])
-}
-
-function dataHandler(dataArray){
+function dataHandler(dataArray) {
   console.log('fired dataHandler');
   console.table(dataArray);
   const range = [...Array(15).keys()];
   const listItems = range.map((item, index) => {
-    const restNum = getRandomIntInclusive(0, dataArray.length-1)
+    const restNum = getRandomIntInclusive(0, dataArray.length - 1);
     return dataArray[restNum];
   });
   console.log(listItems);
@@ -17,11 +12,11 @@ function dataHandler(dataArray){
 // As the last step of your lab, hook this up to index.html
 async function mainEvent() { // the async keyword means we can make API requests
   console.log('script loaded');
-  const form = document.querySelector('.main_form');
+  const form = document.querySelector('.selection-form');
   form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
     submitEvent.preventDefault(); // This prevents your page from refreshing!
     console.log('form submission'); // this is substituting for a "breakpoint"
-    
+
     const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
     const arrayFromJson = await results.json(); // This changes it into data we can use - an object
     console.table(arrayFromJson.data); // this is called "dot notation"
