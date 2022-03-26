5016b791cfeeffc09d24c5fa928bdbd9e33de378
SquishyWizard <cthoma25@terpmail.umd.edu>
2022-03-25 20:07:49 -0400

    Update script.js

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,14 +1,3 @@
-function dataHandler(dataArray) {
-  console.log('fired dataHandler');
-  console.table(dataArray);
-  const range = [...Array(15).keys()];
-  const listItems = range.map((item, index) => {
-    const restoPosition = getRandomIntInclusive(0, dataArray.length - 1)
-    return dataArray[restNum];
-  });
-  console.log(listItems);
-}
-
 function getRandomIntInclusive(min, max) {
   const newMin = Math.ceil(min);
   const newMax = Math.floor(max);
@@ -18,7 +7,7 @@ function getRandomIntInclusive(min, max) {
 }
 
 function restoArrayMake(dataArray) {
-  console.log('fired dataHandler'); 
+  console.log('fired dataHandler');
   console.table(dataArray);
   const range = [...Array(15).keys()];
   const listItems = range.map((item, index) => {
@@ -31,15 +20,12 @@ function restoArrayMake(dataArray) {
 function createHtmlList(collection) {
   console.log('fired HTML creator');
   console.log(collection);
-  const targetList = document.querySelector(".resto-list");
+  const targetList = document.querySelector('.resto-list');
   targetList.innerHTML = '';
   collection.forEach((item) => {
-    const {name} = item;
-    const displayName = name.toLowerCase()
-    const injectThisItem = '<li>${}</li>';
+    const injectThisItem = `<li>${item.name}</li>`;
     targetList.appendChild(injectThisItem);
   });
-  
 }
 // As the last step of your lab, hook this up to index.html
 async function mainEvent() { // the async keyword means we can make API requests
