2dbd01e1842c670e137170f9cf873567107e8834
SquishyWizard <cthoma25@terpmail.umd.edu>
2022-03-26 22:56:44 -0400

    Update script.js

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -46,10 +46,16 @@ async function mainEvent() { // the async keyword means we can make API requests
   if (arrayFromJson.data.length > 0) {
     submit.style.display = 'block';
 
-    
+    const currentArray = [];
     restoName.addEventListener('input', async (event) => {
+      if (currentArray === undefined) { return; }
       console.log(event.target.value);
+      const matchResto = currentArray.filter((item) => {
+        return item.name.includes(event.target.value)
+      });
+      console.log(matchResto);
     });
+    
     form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
       submitEvent.preventDefault(); // This prevents your page from refreshing!
       // console.log('form submission'); // this is substituting for a "breakpoint"
